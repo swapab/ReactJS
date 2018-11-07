a4f38268d999f2e02f0f332c79dbf61fc0e62f05
Swapnil Abnave <imswapab@gmail.com>
2018-11-07 15:08:12 +0100

    Snackbar app with context-api
    Ref: https://itnext.io/understanding-the-react-context-api-through-building-a-shared-snackbar-for-in-app-notifications-6c199446b80c

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { SharedSnackbarProvider } from './SharedSnackbar.context';
+import ButtonA from './ButtonA.component';
+import ButtonB from './ButtonB.component';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+const styles = {
+  app: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div styles={styles.app}>
+        <SharedSnackbarProvider>
+          <ButtonA/>
+          <ButtonB/>
+        </SharedSnackbarProvider>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/context-api/context.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const AppContext = React.createContext();
+
+class AppProvider extends Component {
+  state = {
+    number: 10,
+    inc: () => {
+      this.setState({number: this.state.number + 1})
+    }
+  }
+    render() {
+      return (
+          <AppContext.Provider value={this.state}>
+              {this.props.children}
+          </AppContext.Provider>
+      )
+    }
+}
+
+const Green = () => (
+    <div className="green">
+      <AppContext.Consumer>
+        {(context) => context.number}
+      </AppContext.Consumer>
+    </div>
+)
+
+const Blue = () => (
+    <div className="blue">
+      <AppContext.Consumer>
+        {(context) => <button onClick={context.inc}>INC</button>}
+      </AppContext.Consumer>
+    </div>
+)
+
+class Red extends Component {
+    render() {
+        return (
+            <AppProvider>
+                <div className="red">
+                  <AppContext.Consumer>
+                    {(context) => context.number}
+                  </AppContext.Consumer>
+                    <Blue />
+                </div>
+            </AppProvider>
+        )
+    }
+}
+
+export default Red;
